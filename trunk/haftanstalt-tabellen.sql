3c4eafe2164ca9d5d95aed6a283258c0062bcb6b
taimant@gmail.com <taimant@gmail.com@a91916b0-c0e7-c9f7-87ef-3f13cb7a90e9>
2012-11-28 19:51:55 +0000

    dfasdf

--- trunk/haftanstalt-tabellen.sql
+++ trunk/haftanstalt-tabellen.sql
@@ -19,17 +19,17 @@ define tablespace = "users"
 pause
 
 CREATE TABLE Person
-    (Vorname     VARCHAR(20)     NOT NULL
-    ,Nachname     VARCHAR(20)     NOT NULL
-    ,GebDatum     Date         NOT NULL
-    ,Z_NR         number         NULL
-    ,T_ID         VARCHAR(1)     NULL
-    ,S_Mass     VARCHAR(20)     NULL
-    ,F_Mass     VARCHAR(20)     NULL
-    ,Taetigkeit     VARCHAR(20)     NOT NULL        
-    ,CONSTRAINT     Person_pk_VNNNGB     PRIMARY KEY (Vorname,Nachname,GebDatum)
-    )
-    TABLESPACE &tablespace.
+	(Vorname 	VARCHAR(20) 	NOT NULL
+	,Nachname 	VARCHAR(20) 	NOT NULL
+	,GebDatum 	Date 		NOT NULL
+	,Z_NR 		number 		NULL
+	,T_ID 		VARCHAR(1) 	NULL
+	,S_Mass 	VARCHAR(20) 	NULL
+	,F_Mass 	VARCHAR(20) 	NULL
+	,Taetigkeit 	VARCHAR(20) 	NOT NULL		
+	,CONSTRAINT 	Person_pk_VNNNGB 	PRIMARY KEY (Vorname,Nachname,GebDatum)
+	)
+	TABLESPACE &tablespace.
 ;
 pause
 --
@@ -39,17 +39,17 @@ pause
 pause
 
 CREATE TABLE Behandlung
-    (Medikation     VARCHAR(20)     NOT NULL
-    ,Krankheit    VARCHAR(20)     NOT NULL
-    ,ArztVN        VARCHAR(20)     NOT NULL
-    ,ArztNN        VARCHAR(20)     NOT NULL
-    ,ArztGB        DATE         NOT NULL
-    ,HaeftlingVN    VARCHAR(20)     NOT NULL
-    ,HaeftlingNN    VARCHAR(20)     NOT NULL
-    ,HaeftlingGB    DATE         NOT NULL
-    ,CONSTRAINT    Beh_pk_MedKra        PRIMARY KEY (Medikation,Krankheit,ArztVN,ArztNN,ArztGB)
-    )
-    TABLESPACE &tablespace.
+	(Medikation 	VARCHAR(20) 	NOT NULL
+	,Krankheit	VARCHAR(20) 	NOT NULL
+	,ArztVN		VARCHAR(20) 	NOT NULL
+	,ArztNN		VARCHAR(20) 	NOT NULL
+	,ArztGB		DATE 		NOT NULL
+	,HaeftlingVN	VARCHAR(20) 	NOT NULL
+	,HaeftlingNN	VARCHAR(20) 	NOT NULL
+	,HaeftlingGB	DATE 		NOT NULL
+	,CONSTRAINT	Beh_pk_MedKra		PRIMARY KEY (Medikation,Krankheit,ArztVN,ArztNN,ArztGB)
+	)
+	TABLESPACE &tablespace.
 ;
 pause
 --
@@ -59,15 +59,15 @@ pause
 pause
 
 CREATE TABLE Urteil
-    (Urteilsdatum     DATE         NOT NULL
-    ,Delikt     VARCHAR(20)    NOT NULL
-    ,Jahre        NUMBER(2)    NOT NULL
-    ,HaeftlingVN    VARCHAR(20)     NOT NULL
-    ,HaeftlingNN    VARCHAR(20)     NOT NULL
-    ,HaeftlingGB    DATE         NOT NULL
-    ,CONSTRAINT     Urteil_pk_datumdelikt     PRIMARY KEY (Urteilsdatum,Delikt)
-    )
-    TABLESPACE &tablespace.
+	(Urteilsdatum 	DATE 		NOT NULL
+	,Delikt 	VARCHAR(20)	NOT NULL
+	,Jahre		NUMBER(2)	NOT NULL
+	,HaeftlingVN	VARCHAR(20) 	NOT NULL
+	,HaeftlingNN	VARCHAR(20) 	NOT NULL
+	,HaeftlingGB	DATE 		NOT NULL
+	,CONSTRAINT 	Urteil_pk_datumdelikt 	PRIMARY KEY (Urteilsdatum,Delikt)
+	)
+	TABLESPACE &tablespace.
 ;
 pause
 --
@@ -77,8 +77,8 @@ pause
 pause
 
 CREATE TABLE Trakt
-    (T_ID        VARCHAR(1)    NOT NULL
-    ,CONSTRAINT    Trakt_pk    PRIMARY KEY (T_ID)
+	(T_ID		VARCHAR(1)	NOT NULL
+	,CONSTRAINT	Trakt_pk	PRIMARY KEY (T_ID)
        )
        TABLESPACE &tablespace.
 ;
@@ -90,10 +90,10 @@ pause
 pause
 
 CREATE TABLE Zelle
-    (Z_NR        NUMBER(4)    NOT NULL
-    ,T_ID        VARCHAR(1)    NOT NULL
-    ,Typ        VARCHAR(20)    NOT NULL
-    ,CONSTRAINT    Zelle_pk    PRIMARY KEY (T_ID,Z_NR)
+	(Z_NR		NUMBER(4)	NOT NULL
+	,T_ID		VARCHAR(1)	NOT NULL
+	,Typ		VARCHAR(20)	NOT NULL
+	,CONSTRAINT	Zelle_pk	PRIMARY KEY (T_ID,Z_NR)
        )
        TABLESPACE &tablespace.
 ;
@@ -109,9 +109,9 @@ pause
 --
 
 ALTER TABLE Person ADD 
-    (CONSTRAINT     Person_fk1_T_ID         FOREIGN KEY (T_ID) REFERENCES Trakt(T_ID)
-    ,CONSTRAINT     Person_fk2_Z_NR         FOREIGN KEY (Z_NR) REFERENCES Zelle(T_ID,Z_NR))
-    )
+	(CONSTRAINT 	Person_fk1_T_ID 		FOREIGN KEY (T_ID) REFERENCES Trakt(T_ID)
+	,CONSTRAINT 	Person_fk2_Z_NR 		FOREIGN KEY (Z_NR) REFERENCES Zelle(T_ID,Z_NR))
+	)
 ;
 pause
 --
@@ -120,13 +120,13 @@ pause
 --
 
 ALTER TABLE Behandlung ADD
-    (CONSTRAINT         Beh_fk1_Arzt            FOREIGN KEY (ArztVN,ArztNN,ArztGB) REFERENCES Person(Vorname,Nachname,GebDatum)
-    ,CONSTRAINT      Beh_fk2_ArztNN            FOREIGN KEY (ArztNN) REFERENCES Person(Vorname,Nachname,GebDatum)
-    ,CONSTRAINT      Beh_fk3_ArztGB            FOREIGN KEY (ArztGB) REFERENCES Person(Vorname,Nachname,GebDatum)
-    ,CONSTRAINT      Beh_fk4_HaeftlingVN        FOREIGN KEY (HaeftlingVN) REFERENCES Person(Vorname,Nachname,GebDatum)
-    ,CONSTRAINT      Beh_fk5_HaeftlingNN        FOREIGN KEY (HaeftlingNN) REFERENCES Person(Vorname,Nachname,GebDatum)
-    ,CONSTRAINT      Beh_fk6_HaeftlingGB        FOREIGN KEY (HaeftlingGB) REFERENCES Person(Vorname,Nachname,GebDatum)
-    )
+	(CONSTRAINT    	 Beh_fk1_Arzt			FOREIGN KEY (ArztVN,ArztNN,ArztGB) REFERENCES Person(Vorname,Nachname,GebDatum)
+	,CONSTRAINT      Beh_fk2_ArztNN			FOREIGN KEY (ArztNN) REFERENCES Person(Vorname,Nachname,GebDatum)
+	,CONSTRAINT      Beh_fk3_ArztGB    		FOREIGN KEY (ArztGB) REFERENCES Person(Vorname,Nachname,GebDatum)
+	,CONSTRAINT      Beh_fk4_HaeftlingVN		FOREIGN KEY (HaeftlingVN) REFERENCES Person(Vorname,Nachname,GebDatum)
+	,CONSTRAINT      Beh_fk5_HaeftlingNN		FOREIGN KEY (HaeftlingNN) REFERENCES Person(Vorname,Nachname,GebDatum)
+	,CONSTRAINT      Beh_fk6_HaeftlingGB    	FOREIGN KEY (HaeftlingGB) REFERENCES Person(Vorname,Nachname,GebDatum)
+	)
 ;
 pause
 --
@@ -135,9 +135,9 @@ pause
 --
 
 ALTER TABLE Urteil ADD
-    (CONSTRAINT     Person_fk1_T_ID         FOREIGN KEY (T_ID) REFERENCES Trakt(T_ID)
-    ,CONSTRAINT     Person_fk2_Z_NR         FOREIGN KEY (ZNR) REFERENCES Zelle(T_ID,Z_NR)
-    )
+	(CONSTRAINT 	Person_fk1_T_ID 		FOREIGN KEY (T_ID) REFERENCES Trakt(T_ID)
+	,CONSTRAINT 	Person_fk2_Z_NR 		FOREIGN KEY (ZNR) REFERENCES Zelle(T_ID,Z_NR)
+	)
 ;
 pause
 --
@@ -151,8 +151,8 @@ pause
 --
 
 ALTER TABLE Zelle ADD
-    (CONSTRAINT     Zelle_fk1_T-ID            FOREIGN KEY (T_ID) REFERENCES Trakt(T_ID)
-    )
+	(CONSTRAINT     Zelle_fk1_T-ID			FOREIGN KEY (T_ID) REFERENCES Trakt(T_ID)
+	)
 ;
 pause
 
