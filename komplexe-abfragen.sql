3e92051898972d1a0e1f408e84873f5155fd7e41
Ralf.Zydek <Ralf.Zydek@gmail.com>
2012-12-12 06:47:57 +0000

    blahblupp

--- komplexe-abfragen.sql
+++ komplexe-abfragen.sql
@@ -36,6 +36,22 @@ pause
 pause
 -- Ralf's "Komplizierte" abfrage...
 -- =================================
+-- Der Jüngste und Älteste kranke häftling sollen gesondert überwacht werden
+--
+select r.vorname, r.nachname, r.gebdatum, r.krankheit, r.age, z.z_nr zellennr, z.t_id traktid
+from
+(select x.vorname, x.nachname, x.gebdatum, x.krankheit, (sysdate-x.gebdatum)/365 age, x.t_id, x.z_nr
+from (select p.vorname, p.nachname, p.gebdatum, b.krankheit, p.t_id, p.z_nr
+      from person p join behandlung b 
+      on p.vorname = b.haeftlingvn and p.nachname = b.haeftlingnn and p.gebdatum = haeftlinggb) x
+join (select min(haeftlinggb) mindate, max(haeftlinggb) maxdate
+      from behandlung
+      ) y
+      on y.mindate = x.gebdatum or y.maxdate = x.gebdatum) r
+      join zelle z
+      on r.t_id = z.t_id and r.z_nr = z.z_nr
+;
+--
 --
 pause
 -- Richard's "Komplizierte" abfrage...
